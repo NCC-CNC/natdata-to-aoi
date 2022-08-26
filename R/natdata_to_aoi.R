1c0e57ea4c9ca6fabeffe94f2bf4c4794739d9c6
Dan Wismer <daniel.s.wismer@gmail.com>
2022-08-26 15:55:29 -0600

    align aoi to ncc grid

--- R/natdata_to_aoi.R
+++ R/natdata_to_aoi.R
@@ -1,40 +1,41 @@
 #
 # Authors: Richard Schuster & Dan Wismer
 # Date: August 26th, 2022
-# Description:
-#
-#
+# Description: remember to fill this in
 #
 #
 
 # Import libraries and user functions
 library(sf)
 library(raster)
+library(dplyr)
+library(gdalUtils)
 source("R/fct_res_processing.R")
 
 # Read-in national 1km grid (all of Canada) ----
-ncc_1km <- raster("data/national/Constant_1km.tif")
+ncc_1km <- raster("data/national/boundary.tif")
 ncc_pu <- ncc_1km[]
-ncc_pu <- ncc_pu[!is.na(ncc_pu)]
-
 
 # Read-in area of interest .tiff (aoi) ----
-aoi_pu <- raster("data/aoi/R1km_AOI.tif") %>%
-  getValues() %>%
-  .[!is.na(ncc_pu)] 
+aoi_1km <- raster("data/aoi/R1km_AOI.tif")
+
+# Align aoi to same extent and same number of rows/cols as national grid ----
+gdalUtils::align_rasters(unaligned = "data/aoi/R1km_AOI.tif",
+                         reference = "data/national/boundary.tif",
+                         dstfile = "data/aoi/align/R1km_AOI_Aligned.tif")
+
+# Get numeric vector of aoi planning units 
+aoi_pu <- raster("data/aoi/align/R1km_AOI_Aligned.tif") %>%
+  getValues()  %>% .[!is.na(ncc_pu)] 
 # Reclass na to 0
 aoi_pu <- ifelse(!is.na(aoi_pu), 1, 0)
 
 # Read-in national data ----
 SAR <- readRDS("data/national/rij_SAR.rds") 
-# amph <- readRDS( "data/national/rij_amph.rds") 
-# bird <- readRDS( "data/national/rij_bird.rds") 
-# mamm <- readRDS( "data/national/rij_mamm.rds") 
-# rept <- readRDS( "data/national/rij_rept.rds") 
-# ecor <- readRDS( "data/national/rij_ecor.rds") 
-# NSC_END <- readRDS( "data/national/rij_NSC_END.rds") 
-# NSC_SAR <- readRDS( "data/national/rij_NSC_SAR.rds") 
-# NSC_SPP <- readRDS( "data/national/rij_NSC_SPP.rds")
+NSC_END <- readRDS( "data/national/rij_NSC_END.rds")
+NSC_SAR <- readRDS( "data/national/rij_NSC_SAR.rds")
+NSC_SPP <- readRDS( "data/national/rij_NSC_SPP.rds")
 
 # Matrix multiplication 
-pu_SAR <- SAR %*% aoi_pu 
+SAR_X_aoi_pu <- SAR %*% aoi_pu
+SAR_aoi_pu <- SAR_X_aoi_pu[rowSums(SAR_X_aoi_pu) > 0, ]

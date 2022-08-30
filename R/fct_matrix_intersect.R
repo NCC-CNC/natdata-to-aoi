d37a1c5ec36f164b6164e733bc9fa4a19da42663
Dan Wismer <daniel.s.wismer@gmail.com>
2022-08-30 08:45:30 -0600

    `matrix_intersect` function documentation

--- R/fct_matrix_intersect.R
+++ R/fct_matrix_intersect.R
@@ -1,3 +1,15 @@
+#' Finds national data records that overlap AOI
+#' 
+#' @description
+#' The matrix_intersect function uses indexing to subset rows that are greater
+#' than 0.
+#' 
+#' @param natdata_rij a sparse matrix of class [dgCMatrix]. National data,
+#' example `rij_NSC_SAR.rds`
+#' 
+#' @param aoi_rij a sparse matrix of class [dgCMatrix]. Area of interest 
+#' `rij_matrix`
+
 matrix_intersect <- function(natdata_rij, aoi_rij) {
   # append AOI and Idx to SAR matrix
   natdata_aoi_ncc <- rbind(natdata_rij, aoi_rij) 
